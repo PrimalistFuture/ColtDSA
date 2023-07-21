c4df78e29a03eb10444220a038eec2bc0b6fa9b4
Vaughn <vseekamp@gmail.com>
2023-07-21 08:46:01 -0700

    up to space complexity

--- 02-BigO/Demo.js
+++ 02-BigO/Demo.js
@@ -11,8 +11,8 @@ function revString(str) {
 
 function revString2(str) {
   let revString = '';
-  for (let i = str.length-1; i >= 0; i--) {
-    revString+=str[i];
+  for (let i = str.length - 1; i >= 0; i--) {
+    revString += str[i];
   }
   return revString;
 }
@@ -41,5 +41,19 @@ function addToN2(n) {
 // Below is wayyyyy faster because for even small values of n, the first solution has to do more operations. Whereas below is alway just 3 - multiply, add, divide.
 //O(1)
 function addToN3(n) {
-  return n * (n+1) / 2;
+  return n * (n + 1) / 2;
+}
+
+// Big O is worst case scenario. The runtime for this then is O(n) because the 5 doesn't matter as the n grows.
+function logAtLeast5(n) {
+  for (let i = 0; i <= Math.max(5, n); i++) {
+    console.log(i);
+  }
+}
+
+// Because we can only log a max of 5, n no longer matters. So now we have O(1)
+function logAtMost5(n) {
+  for (let i = 0; i <= Math.min(5, n); i++) {
+    console.log(i);
+  }
 }
