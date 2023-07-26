97532b23da4e1319a51e438c00c46507fdb0fe01
Vaughn <vseekamp@gmail.com>
2023-07-26 15:00:54 -0700

    i got smoked by sliding windows

--- 06-Practice/Demo.js
+++ 06-Practice/Demo.js
@@ -166,3 +166,84 @@ function isSubsequence(str1, str2) {
     }
     return index === str1.length;
 }
+
+// Sliding Window - maxSubarraySum
+// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+// Note that a subarray must consist of consecutive elements from the original array.In the first example below, [100, 200, 300] is a subarray of the original array, but[100, 300] is not.
+
+// maxSubarraySum([100, 200, 300, 400], 2) // 700
+// maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)  // 39 
+// maxSubarraySum([-3, 4, 0, -2, 6, -1], 2) // 5
+// maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2) // 5
+// maxSubarraySum([2, 3], 3) // null
+
+// Constraints:
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+function maxSubarraySum(ints, n) {
+    if (n > ints.length) {
+        return null;
+    }
+    let max = 0;
+    let temp = 0;
+    for (let i = 0; i < n; i++) {
+        max += ints[i];
+    }
+    temp = max;
+    for (let i = n; i < ints.length; i++) {
+        temp = temp - ints[i - n] + ints[i];
+        if (temp > max) {
+            max = temp;
+        }
+    }
+    return max;
+}
+
+// Mostly got this one because it is the same problem as we had earlier and I remembered how to do it. I wonder how another sliding window problem would go.
+
+
+// Sliding Window - minSubArrayLen
+// Write a function called minSubArrayLen which accepts two parameters - an array of positive integers and a positive integer.
+// This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
+
+// Examples:
+// minSubArrayLen([2, 3, 1, 2, 4, 3], 7) // 2 -> because [4,3] is the smallest subarray
+// minSubArrayLen([2, 1, 6, 5, 4], 9) // 2 -> because [5,4] is the smallest subarray
+// minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52) // 1 -> because [62] is greater than 52
+// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39) // 3
+// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55) // 5
+// minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11) // 2
+// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95) // 0
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+function minSubArrayLen(nums, sum) {
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+
+    while (start < nums.length) {
+        // if current window doesn't add up to the given sum then
+        // move the window to right
+        if (total < sum && end < nums.length) {
+            total += nums[end];
+            end++;
+        }
+        // if current window adds up to at least the sum given then
+        // we can shrink the window 
+        else if (total >= sum) {
+            minLen = Math.min(minLen, end - start);
+            total -= nums[start];
+            start++;
+        }
+        // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+        else {
+            break;
+        }
+    }
+    return minLen === Infinity ? 0 : minLen;
+}
