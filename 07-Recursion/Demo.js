ce203d8b660dc71b51bd7c6c2edeb8cda750ac5b
Vaughn <vseekamp@gmail.com>
2023-07-31 14:22:19 -0700

    pure recursion

--- 07-Recursion/Demo.js
+++ 07-Recursion/Demo.js
@@ -66,3 +66,47 @@ function factorial(num) {
 }
 
 factorial(5)
+
+
+// helper method recursion
+// result needs to be stored outside of the recursive part of the function so that it is not reset with every invocation. So we will wrap this in another function.
+function collectOddValues(arr) {
+  let result = [];
+  function helper(helperInput) {
+    if (helperInput.length === 0) {
+      return;
+    }
+    if (helperInput[0] % 2 === 1) {
+      result.push(helperInput[0])
+    }
+    helper(helperInput.slice(1))
+  }
+  helper(arr)
+  return result;
+}
+
+collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+
+// pure recursion
+
+function collectOdds(arr) {
+  // this is the bit that is far trickier than it looks. The results variable is created everytime, but it is concated with all other results variables.
+  let results = [];
+
+  if (arr.length === 0) {
+    return results;
+  }
+  if (arr[0] % 2 === 1) {
+    results.push(arr[0]);
+  }
+  results = results.concat(collectOdds(arr.slice(1)))
+  return results;
+}
+
+// collectOdds([1,2,3,4,5])
+  // [1].concat(collectOdds([2,3,4,5]))
+    // [].concat(collectOdds([3,4,5]))
+      // [3].concat(collectOdds([4,5]))
+        // [].concat(collectOdds([5]))
+          // [5].concat(collectOdds([]))
