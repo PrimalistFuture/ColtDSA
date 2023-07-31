5519dcd1e886c16b0b597d6c4a302264dd6968b7
Vaughn <vseekamp@gmail.com>
2023-07-31 13:44:44 -0700

    few recursion problems in demo

--- 07-Recursion/Demo.js
+++ 07-Recursion/Demo.js
@@ -39,3 +39,30 @@ function countDown(num) {
 }
 
 countDown(5);
+
+// more
+
+function sumRange(num) {
+  // base case
+  if (num === 1) {
+    return 1;
+  }
+
+  return num + sumRange(num - 1);
+}
+
+sumRange(3)
+// return 3 + sumRange(2)
+// return 2 + sumRange(1)
+// return 1
+// 6 
+
+
+function factorial(num) {
+  if (num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+factorial(5)
