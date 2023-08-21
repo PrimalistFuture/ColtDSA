14d12356ae705888977eef5cbafba24418b68fce
Vaughn <vseekamp@gmail.com>
2023-08-21 14:48:50 -0700

    trees

---
+++ 22-BinarySearchTrees/demo.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// binary search tree implementation
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    // inserts a node of a given value into the correct spot of the tree. Returns the tree.
+    // done interatively, but could have been done recursively.
+    insert(val) {
+        let newNode = new Node(val);
+
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        }
+        let current = this.root;
+        while (true) {
+            if (val === current.val) {
+                return `dup val`;
+            }
+            if (val < current.val) {
+                if (!current.left) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if (!current.right) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(10);
+tree.insert(7);
