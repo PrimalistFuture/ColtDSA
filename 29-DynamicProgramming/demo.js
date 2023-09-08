918b00e7475ec796fcf04675f02fed807eea1309
Vaughn <vseekamp@gmail.com>
2023-09-08 09:52:33 -0700

    done?

--- 29-DynamicProgramming/demo.js
+++ 29-DynamicProgramming/demo.js
@@ -14,8 +14,46 @@ function fib(num) {
         return 1;
     }
     return fib(num - 1) + fib(num - 2);
+
 }
 
-// this works but isn't very performant. Going from fib(6) to fib(7) is a huge jump, because it involves everything of fib(6) and of fib(5). Very large, very quickly. O(2^n). Bad.
+// this works but isn't very performant. Going from fib(6) to fib(7) is a huge jump, because it involves everything of fib(6) and of fib(5), neither of which 'remember' that fib(6) involves entirely solving fib(5). Very large, very quickly. O(2^n). Bad.
 
+// Memoized Variant
+// by filling out memo beforhand, we don't need our base case anymore.
+function fib(num, memo = [undefined, 1, 1]) {
+    if (memo[num] !== undefined) {
+        return memo[num];
+    }
+    // recursive call
+    let result = fib(num - 1, memo) + fib(num - 2, memo);
+    // stores the recursive call in our memo to be passed around
+    memo[num] = result;
+    return result;
+}
 
+/// Improves the performance dramatically. Now fib(100) is nearly instant instead of crashing my browser. Could be improved further by storing memo elsewhere so it doesn't need recalculating here.
+
+// Like this:
+let fibMemo = { 0: undefined, 1: 1, 2: 1 }
+
+function fib(num) {
+    if (fibMemo[num] !== undefined) {
+        return fibMemo[num];
+    }
+    fibMemo[num] = fib(num - 1) + fib(num - 2);
+    return fibMemo[num];
+}
+
+// Tabulated Variant
+// same result, but now works from the bottom up, calculating fib(3) -> fib(4) so on.
+function fib(num) {
+    if (num <= 2) {
+        return 1;
+    }
+    let fibNums = [0, 1, 1];
+    for (let i = 3; i <= num; i++) {
+        fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+    }
+    return fibNums[num];
+}
